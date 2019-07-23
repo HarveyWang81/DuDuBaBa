7ddc1f9d69922c49b679e5438008b495db9db162
Lin Wang <lin.wang@t-systems.com>
2019-07-23 16:26:41 +0800

    1

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,11 @@
 """
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask('__name__')
 app.config.from_object('app.config')
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 
 from routes import views
